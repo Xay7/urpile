0fef2106bdf45720aa2c643adde1d11ca1a02f34
Emil <emilian.cw@gmail.com>
2019-09-28 11:16:08 +0200

    Added necessary stuff

--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -4,10 +4,11 @@ import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import Checkbox from "../../components/Checkbox/Checkbox";
 import axios from "axios";
-import { RouteComponentProps } from "react-router-dom";
+import { useHistory, RouteComponentProps, Link } from "react-router-dom";
 
 const Login: React.FC<RouteComponentProps> = props => {
   const [checkboxChecked, setCheckboxChecked] = useState(false);
+  const history = useHistory();
 
   const formHandler = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
@@ -20,21 +21,32 @@ const Login: React.FC<RouteComponentProps> = props => {
       if (remember) {
         localStorage.setItem("remember", "true");
       }
-      props.history.push("/dashboard");
+      history.push("/dashboard");
     } catch (error) {}
   };
 
   return (
     <StyledLogin>
       <Container>
-        <Header>Login</Header>
+        <Header>Sign in</Header>
         <Form onSubmit={formHandler}>
           <Input type="email" placeholder="Email" autoComplete="on" />
           <Input type="password" placeholder="Password" autoComplete="on" />
-          <Checkbox isChecked={checkboxChecked} handleChange={() => setCheckboxChecked(!checkboxChecked)} />
-          <Button>Login</Button>
+          <Checkbox
+            checked={checkboxChecked}
+            onChange={() => setCheckboxChecked(!checkboxChecked)}
+            label={"Remember me"}
+          />
+          {props.location.state && props.location.state.registered && <div>Success, you can login.</div>}
+          <Button>Continue</Button>
         </Form>
       </Container>
+      <span style={{ fontSize: "1.3rem", margin: "10px" }}>
+        Don&apos;t have an account?&nbsp;
+        <Link to="/register" style={{ textDecoration: "none", color: "blue" }}>
+          Sign up
+        </Link>
+      </span>
     </StyledLogin>
   );
 };
@@ -52,6 +64,8 @@ const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  background-color: white;
+  box-shadow: ${props => props.theme.shadows["3dp"]};
 `;
 
 const Form = styled.form`
@@ -59,14 +73,16 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 0 30px 24px 30px;
 
-  div {
+  label {
     align-self: flex-start;
   }
 `;
 
 const Header = styled.h3`
   font-size: 2.4rem;
+  margin-bottom: 9px;
 `;
 
 export default Login;
--- client/src/views/Register/Register.tsx
+++ client/src/views/Register/Register.tsx
@@ -1,32 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../components/Input/Input";
 import styled from "styled-components";
 import Button from "../../components/Button/Button";
 import axios from "axios";
-import { RouteComponentProps } from "react-router";
+import { useHistory, Link } from "react-router-dom";
 
-const Register: React.FC<RouteComponentProps> = props => {
+const Register: React.FC = props => {
+  const history = useHistory();
+  const [errors, setErrors] = useState({
+    isValid: false,
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
   const formHandler = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     const username = (e.currentTarget[0] as HTMLInputElement).value;
     const email = (e.currentTarget[1] as HTMLInputElement).value;
     const password = (e.currentTarget[2] as HTMLInputElement).value;
+    const confirmPassword = (e.currentTarget[3] as HTMLInputElement).value;
+
+    validate(username, email, password, confirmPassword);
+    if (!errors.isValid) {
+      return;
+    }
+
     try {
-      await axios.post("/users/register", { username, email, password });
-      props.history.push("/login");
+      await axios.post("/users/register", { username, email, password, confirmPassword });
+      history.push({
+        pathname: "login",
+        state: { registered: true }
+      });
     } catch (error) {
       console.log(error.response.data);
     }
   };
 
+  const validate = (username: string, email: string, password: string, confirmPassword: string) => {
+    const errors = {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      isValid: true
+    };
+
+    const isEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+
+    if (username.length < 3) {
+      errors.isValid = false;
+      errors.username = "Username must be atleast 3 characters long";
+    }
+
+    if (!isEmail) {
+      errors.isValid = false;
+      errors.email = "Email must be valid";
+    }
+    if (password.length < 6) {
+      errors.isValid = false;
+      errors.password = "Password must be atleast 6 characters long";
+    }
+    if (confirmPassword !== password || confirmPassword.length === 0) {
+      errors.isValid = false;
+      errors.confirmPassword = "Passwords must match";
+    }
+    return setErrors(errors);
+  };
+
   return (
     <StyledRegister>
-      <Form onSubmit={formHandler}>
-        <Input type="text" placeholder="Username" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" autoComplete="off" />
-        <Button>Register</Button>
-      </Form>
+      <Container>
+        <Header>Create account</Header>
+        <Form onSubmit={formHandler}>
+          <Input
+            type="text"
+            placeholder="Username"
+            error={errors.username ? true : false}
+            errorMessage={errors.username}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            error={errors.email ? true : false}
+            errorMessage={errors.email}
+            autoComplete="new-password"
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            error={errors.password ? true : false}
+            errorMessage={errors.password}
+          />
+          <Input
+            type="password"
+            placeholder="Confirm password"
+            error={errors.confirmPassword ? true : false}
+            errorMessage={errors.confirmPassword}
+          />
+          <Button>Continue</Button>
+        </Form>
+      </Container>
+      <span style={{ fontSize: "1.3rem", margin: "10px" }}>
+        Arleady registered?&nbsp;
+        <Link to="/login" style={{ textDecoration: "none", color: "blue" }}>
+          Sign in
+        </Link>
+      </span>
     </StyledRegister>
   );
 };
@@ -39,15 +122,33 @@ const StyledRegister = styled.div`
   justify-content: center;
 `;
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 0 30px 24px 30px;
+  box-shadow: ${props => props.theme.shadows["3dp"]};
+  border-radius: 5px;
+`;
+
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: white;
+
+  button {
+    margin-top: 24px;
+  }
 `;
 
-const Info = styled.p`
-  font-size: 1.2rem;
+const Header = styled.h3`
+  font-size: 2.4rem;
+  margin-bottom: 9px;
+  color: ${props => props.theme.black};
 `;
 
 export default Register;
