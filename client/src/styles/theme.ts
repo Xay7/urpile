d791b87e3fb13f95f7be6237ebb95eff859f772a
Emil <emilian.cw@gmail.com>
2019-09-28 11:15:34 +0200

    Add primary settings

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,5 +1,16 @@
 const theme = {
-  main: "mediumseagreen"
+  main: "mediumseagreen",
+  primary: "#0984e3",
+  error: "#c40000",
+  black: "#333",
+  shadows: {
+    "1dp":
+      "0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)",
+    "2dp":
+      "0px 1px 5px 0px rgba(0, 0, 0, 0.2),0px 2px 2px 0px rgba(0, 0, 0, 0.14),0px 3px 1px -2px rgba(0, 0, 0, 0.12);",
+    "3dp":
+      "0px 1px 8px 0px rgba(0, 0, 0, 0.2),0px 3px 4px 0px rgba(0, 0, 0, 0.14),0px 3px 3px -2px rgba(0, 0, 0, 0.12);"
+  }
 };
 
 export default theme;
