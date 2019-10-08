07b6a8a9936a4dc8245a9a292751028a8d1bca75
Emil <emilian.cw@gmail.com>
2019-10-08 21:43:36 +0200

    Add 404, css, lazy load dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,13 @@ import { hot } from "react-hot-loader/root";
 import { Switch, Route } from "react-router-dom";
 import Login from "./views/Login/Login";
 import Landing from "./views/Landing/Landing";
-import Dashboard from "./views/Dashboard/Dashboard";
 import Register from "./views/Register/Register";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Navbar from "./components/Navbar/Navbar";
 import Passwords from "./views/Passwords/Passwords";
+import Calendar from "./views/Calendar/Calendar";
+
+const Dashboard = React.lazy(() => import("./views/Dashboard/Dashboard"));
 
 const App: React.FC = () => {
   return (
@@ -36,13 +38,16 @@ const DashboardLayout = () => {
     <>
       <Navbar />
       <Main>
-        <Switch>
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/dashboard/notes" component={Dashboard} />
-          <Route exact path="/dashboard/calendar" component={Dashboard} />
-          <Route exact path="/dashboard/passwords" component={Passwords} />
-          <Route exact path="/dashboard/logout" component={Dashboard} />
-        </Switch>
+        <React.Suspense fallback={<div>loading</div>}>
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/dashboard/notes" component={Dashboard} />
+            <Route exact path="/dashboard/calendar" component={Calendar} />
+            <Route exact path="/dashboard/passwords" component={Passwords} />
+            <Route exact path="/dashboard/logout" component={Dashboard} />
+            <Route path="/dashboard/*" component={() => <div>Make 404 page here</div>} />
+          </Switch>
+        </React.Suspense>
       </Main>
     </>
   );
@@ -51,8 +56,8 @@ const DashboardLayout = () => {
 const Main = styled.main`
   width: calc(100vw - 240px);
   height: 100%;
+  padding: 30px;
   margin-left: 240px;
-  padding: 50px;
   box-sizing: border-box;
 `;
 
