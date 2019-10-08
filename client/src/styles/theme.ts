f7e91db20f846d4aec015b361f1f83370f33f5f0
Emil <emilian.cw@gmail.com>
2019-10-08 21:43:22 +0200

    Reset css

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,10 +1,12 @@
 const theme = {
   main: "mediumseagreen",
   primary: "#0984e3",
-  success: "green",
+  success: "#4BB543",
   error: "#ff0033",
   black: "#333",
   white: "#fbfbfb",
+  grey: "#aaa",
+  red: "red",
   shadows: {
     "1dp":
       "0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)",
